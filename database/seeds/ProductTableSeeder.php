a537158476c00e9cd1a261b4013ab4598f57c3dd
leonardozaneladias <leonardozaneladias@gmail.com>
2019-08-03 14:53:34 -0300

    Update ProductTableSeeder.php

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -13,7 +13,7 @@ class ProductTableSeeder extends Seeder
     {
         DB::table('products')->insert([
             'establishment_id' => 4,
-            'category_id' => rand(4),
+            'category_id' => 1,
             'title' => 'Pizza de Mussarela',
             'picture' => 'http://leoztech.com/imgs/products/pizza-mussarela1.jpg',
             'description' => "blablablalbal",
@@ -24,7 +24,7 @@ class ProductTableSeeder extends Seeder
 
         DB::table('products')->insert([
             'establishment_id' => 4,
-            'category_id' => rand(1,5),
+            'category_id' => 1,
             'title' => 'Pizza de Calabresa',
             'picture' => 'http://leoztech.com/imgs/products/calabresa1.jpg',
             'description' => "blablablalbal",
@@ -35,7 +35,7 @@ class ProductTableSeeder extends Seeder
 
         DB::table('products')->insert([
             'establishment_id' => 4,
-            'category_id' => rand(1,5),
+            'category_id' => 1,
             'title' => 'Pizza de Portuguesa',
             'picture' => 'http://leoztech.com/imgs/products/pizza-portuguesa.jpg',
             'description' => "blablablalbal",
@@ -46,7 +46,7 @@ class ProductTableSeeder extends Seeder
 
         DB::table('products')->insert([
             'establishment_id' => 4,
-            'category_id' => rand(1,5),
+            'category_id' => 1,
             'title' => 'Pizza de Quatro Queijos',
             'picture' => 'http://leoztech.com/imgs/products/quatro-queijos.jpg',
             'description' => "blablablalbal",
@@ -57,7 +57,7 @@ class ProductTableSeeder extends Seeder
 
         DB::table('products')->insert([
             'establishment_id' => 4,
-            'category_id' => rand(1,5),
+            'category_id' => 1,
             'title' => 'Pizza de Marguerita',
             'picture' => 'http://leoztech.com/imgs/products/marguerita.jpg',
             'description' => "blablablalbal",
